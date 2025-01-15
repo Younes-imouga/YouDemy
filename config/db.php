eda2b56eac42973fe602804fb9a9a94a42c2e3b6
Younes <younes.imouga@gmail.com>
2025-01-15 14:30:57 +0100

    added database connection

--- config/db.php
+++ config/db.php
@@ -7,7 +7,7 @@ class Db {
   public function __construct()
   {
     try {
-      $this->conn = new PDO("mysql: host = localhost; dbname = bank","root","");
+      $this->conn = new PDO("mysql:host=localhost;dbname=course_online", "root", "");
       $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch(PDOException $e) {
       echo "Connection failed: " . $e->getMessage();
